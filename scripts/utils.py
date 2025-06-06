bed8667fe04da2b6abd6ab7a38c2493dc3780a93
Harry Hoang <theanhhoang0201@gmail.com>
2025-06-05 22:23:19 -0700

    added sqlalchemy engine and make into python package

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,7 +1,7 @@
 import os 
 from dotenv import load_dotenv
 import mysql.connector
-
+from sqlalchemy import create_engine
 def db_connect():
     load_dotenv()
     return mysql.connector.connect(
@@ -10,3 +10,7 @@ def db_connect():
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME")
     )
+    
+def get_engine():
+    load_dotenv()
+    return create_engine(os.getenv("ENGINE_URL"))
